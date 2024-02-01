ac6a1c4f8c8270220acd0f4d18111527d383ad3a
imFaizanAhmed <imfaizanahmed6@gmail.com>
2024-02-02 00:48:18 +0500

    Nav bar is implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,35 @@
-import { Images } from "./assets/index";
-import './input.css'
-import FirstComponent from "./components";
+import "./input.css";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import PrimarySearchAppBar from "./components/Header";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#fff",
+    },
+    secondary: {
+      main: "#607d8b"
+    },
+    common: {
+      white: "#546e7a"
+    },
+    background: {
+      default: '#000'
+    }
+    // mode: 'dark'
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="text-right font-bold underline">Hello world!</h1>
-        <img src={Images.lightLogo} className="App-logo" alt="logo" />
-        <FirstComponent />
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <header className="App-header">
+          <PrimarySearchAppBar />
+          <h1 className="text-right font-bold underline">Hello world!</h1>
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
 
