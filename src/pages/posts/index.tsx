67b2f86c616053f7116e0c660f7ed1a07288579e
ImFaizanAhmed <imfaizanahmed6@gmail.com>
2024-05-17 12:31:10 +0500

    infinite scroll functionality is added

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -2,10 +2,11 @@ import { useInfiniteQuery } from "react-query";
 import RecipeReviewCard from "../../components/post";
 import axiosInstance from "../../apis/axios";
 import { ApiResponse } from "../../types/post.type";
+import { useEffect, useRef } from "react";
 
 const Posts = () => {
   const fetchPosts = async ({ pageParam = 1 }) => {
-    const limit = 2; // Define your limit here
+    const limit = 2;
     const response = await axiosInstance.get<ApiResponse>(
       `post/all?page=${pageParam}&limit=${limit}`
     );
@@ -34,6 +35,33 @@ const Posts = () => {
     isFetchingNextPage,
   } = useFetchPosts();
 
+  const observerElem = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    // infinite scroll functionality
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting && hasNextPage) {
+          fetchNextPage();
+        }
+      },
+      {
+        rootMargin: '100px',
+      }
+    );
+
+    if (observerElem.current) {
+      observer.observe(observerElem.current);
+    }
+
+    return () => {
+      if (observerElem.current) {
+        observer.unobserve(observerElem.current);
+      }
+    };
+  }, [fetchNextPage, hasNextPage]);
+
+
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error: {error.message}</div>;
 
@@ -53,16 +81,9 @@ const Posts = () => {
           );
         })}
       </div>
-      <button
-        onClick={() => fetchNextPage()}
-        disabled={!hasNextPage || isFetchingNextPage}
-      >
-        {isFetchingNextPage
-          ? "Loading more..."
-          : hasNextPage
-          ? "Load More"
-          : "No More Posts"}
-      </button>
+      <div ref={observerElem} style={{ height: '20px' }} />
+      {isFetchingNextPage && <div>Loading more...</div>}
+      {!hasNextPage && <div>No More Posts</div>}
     </>
   );
 };
